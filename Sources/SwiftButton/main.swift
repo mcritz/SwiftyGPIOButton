43ab7f4eadae81e8504a1927aabc42ba050d28eb
Michael Critz <github@michaelcritz.com>
2019-08-13 08:07:15 -0700

    proper error type

--- Sources/SwiftButton/main.swift
+++ Sources/SwiftButton/main.swift
@@ -4,8 +4,12 @@ print("Startup")
 
 let gpios = SwiftyGPIO.GPIOs(for:.RaspberryPiPlusZero)
 
+enum HardwareError: Error {
+    case noAccess, unknown
+}
+
 var maybegp = gpios[.P10]
-guard var gp: GPIO = maybegp else { throw("Couldn’t access GPIO P10") }
+guard var gp: GPIO = maybegp else { throw(HardwareError.noAccess) }
 gp.direction = .IN
 gp.pull = .down
 

d1c1b8134f74f481440b2a22a4d2f053ea9b193e
Michael Critz <github@michaelcritz.com>
2019-08-16 19:45:55 -0700

    Switched to blinky

--- Sources/SwiftButton/main.swift
+++ Sources/SwiftButton/main.swift
@@ -1,20 +1,29 @@
+import Foundation
 import SwiftyGPIO
-
-print("Startup")
-
-let gpios = SwiftyGPIO.GPIOs(for:.RaspberryPiPlusZero)
 
 enum HardwareError: Error {
     case noAccess, unknown
 }
 
-var maybegp = gpios[.P10]
-guard var gp: GPIO = maybegp else { throw(HardwareError.noAccess) }
-gp.direction = .IN
-gp.pull = .down
+print("Startup")
 
-gp.onChange { gpio in
-    print("Value changed. Now: " + String(gpio.value))
+let gpios = SwiftyGPIO.GPIOs(for:.RaspberryPiPlusZero)
+
+var maybegp = gpios[.P10]
+guard var gp: GPIO = maybegp else {
+    print("failed no access")
+    throw(HardwareError.noAccess)
 }
+
+gp.direction = .OUT
+
+for ii in 0...20 {
+    gp.value = ii % 2
+    sleep(2)
+}
+
+//gp.onChange { gpio in
+//    print("Value changed. Now: " + String(gpio.value))
+//}
 
 
